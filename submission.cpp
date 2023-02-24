3bc9e23b174fad52045f9fdfe19fe3424cf24c13
Sarthak Batham <sarthakbatham26@gmail.com>
2023-02-24 18:01:34 +0530

    41A accepted

--- submission.cpp
+++ submission.cpp
@@ -1,4 +1,30 @@
-#include "template.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define YES "YES"
+#define NO "NO"
+#define ll long long
+
+inline void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+inline void clearCin(int delim = '\n') {
+    cin.ignore(numeric_limits<streamsize>::max(), delim);
+    cin.clear();
+}
+
+
+void solve();
+
+int main()
+{
+    fast_io();
+    solve();
+    cout << endl;
+}
 
 void solve() {
     string ber, bir;
