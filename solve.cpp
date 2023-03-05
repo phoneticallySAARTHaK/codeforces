4f321d60e2888891003223ca6a2630fa4d73bc31
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-06 02:42:13 +0530

    solve 677A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,11 @@
 #include "template.h"
 
 void solve() {
-  //
+  auto [n, h] = getInput<int, int>();
+
+  auto heights{getInput<int>(n)};
+
+  cout << accumulate(
+      begin(heights), end(heights), 0,
+      [&h](cauto acc, cauto el) { return acc + (el <= h ? 1 : 2); });
 }
---
+++ submission.cpp
@@ -0,0 +1,101 @@
+#pragma once
+#include <bits/stdc++.h>
+using std::apply;
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::conjunction;
+using std::integral;
+using std::ios_base;
+using std::is_fundamental;
+using std::is_integral;
+using std::istream_iterator;
+using std::map;
+using std::numeric_limits;
+using std::pair;
+using std::setw;
+using std::streamsize;
+using std::string;
+using std::ref;
+using std::tuple;
+using std::vector;
+using std::semiregular;
+using std::signed_integral;
+using std::accumulate;
+
+namespace rs = std::ranges;
+namespace vs = std::views;
+using namespace rs;
+using namespace vs;
+
+#define endl '\n'
+#define YES "YES"
+#define NO "NO"
+#define ll long long
+#define cauto const auto
+
+inline void fast_io() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+}
+
+inline void clear(int delim = '\n') {
+  cin.ignore(numeric_limits<streamsize>::max(), delim);
+  cin.clear();
+}
+
+template <typename T = int, typename N = int>
+  requires is_fundamental<T>::value && is_integral<N>::value
+void discard(N n = 1) {
+  // https://stackoverflow.com/questions/62940748/why-stdistream-iterator-with-multiple-copy-n-always-writes-firs-value
+  // This comes down to a perhaps unintuitive fact of istream_iterator:
+  //    it doesn't read when you dereference it, but instead when you advance (or construct) it.
+  auto it{istream_iterator<T>(cin)};
+  advance(it, n - 1, istream_iterator<T>{});
+}
+
+// Fold expression, parameter
+// packshttps://www.scs.stanford.edu/~dm/blog/param-pack.html
+// (See testGetInput for usage)
+template <semiregular ...T>
+auto getInput() {
+  tuple<T...> tpl;
+  apply([](auto& ...args) { (cin >> ... >> args); }, tpl);
+
+  assert(cin.good() && "Input failure");
+
+  return tpl;
+}
+
+template<semiregular T>
+vector<T> getInput(signed_integral auto n) {
+  vector<T> vec(n);
+  copy_n(istream_iterator<T>(cin), n, begin(vec));
+  return vec;
+}
+
+
+
+
+
+void solve();
+
+int main() {
+  try {
+    fast_io();
+    solve();
+    cout << endl;
+  } catch (...) {
+    cerr << "Catch-all handler" << endl;
+  }
+}
+
+void solve() {
+  auto [n, h] = getInput<int, int>();
+
+  auto heights{getInput<int>(n)};
+
+  cout << accumulate(
+      begin(heights), end(heights), 0,
+      [&h](cauto acc, cauto el) { return acc + (el <= h ? 1 : 2); });
+}
