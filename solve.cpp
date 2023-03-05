1eef99311e410c59b490596662e5f8d6642bc2ea
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-06 04:42:54 +0530

    solve 1030A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,10 @@
 #include "template.h"
 
 void solve() {
-  //
+  using is_it = istream_iterator<int>;
+  discard();
+  is_it end{};
+  // if the istream_iterator doesn't equal 'end', then '1' was found, hence the
+  // problem is hard.
+  cout << ((find(is_it(cin), end, 1) != end) ? "HARD" : "EASY");
 }
