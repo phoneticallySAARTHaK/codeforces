303825bf2fa203f8b5c2c0fe262ed0a85a8098dc
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-06 04:09:14 +0530

    solve 271A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,12 @@
 #include "template.h"
 
+bool isUnique(integral auto n) {
+  auto digits{splitDigitsReversed(n)};
+  set<int> uniqDigits{all(digits)};
+  return ssize(digits) == ssize(uniqDigits);
+}
+
 void solve() {
-  //
+  auto [y] = getInput<int>();
+  cout << *find_if(iota(y + 1), [](auto n) { return isUnique(n); });
 }
---
+++ submission.cpp
@@ -0,0 +1,115 @@
+#pragma once
+#include <bits/stdc++.h>
+using std::accumulate;
+using std::apply;
+using std::cerr;
+using std::cin;
+using std::conjunction;
+using std::cout;
+using std::integral;
+using std::ios_base;
+using std::is_fundamental;
+using std::is_integral;
+using std::istream_iterator;
+using std::map;
+using std::numeric_limits;
+using std::pair;
+using std::ref;
+using std::semiregular;
+using std::setw;
+using std::signed_integral;
+using std::streamsize;
+using std::string;
+using std::tuple;
+using std::vector;
+using std::set;
+using std::unreachable_sentinel;
+
+namespace rs = std::ranges;
+namespace vs = std::views;
+using namespace rs;
+using namespace vs;
+
+#define endl '\n'
+#define YES "YES"
+#define NO "NO"
+#define ll long long
+#define cauto const auto
+#define all(x) std::begin(x), std::end(x)
+
+inline void fast_io() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+}
+
+inline void clear(int delim = '\n') {
+  cin.ignore(numeric_limits<streamsize>::max(), delim);
+  cin.clear();
+}
+
+template <typename T = int, typename N = int>
+  requires is_fundamental<T>::value && is_integral<N>::value
+void discard(N n = 1) {
+  // https://stackoverflow.com/questions/62940748/why-stdistream-iterator-with-multiple-copy-n-always-writes-firs-value
+  // This comes down to a perhaps unintuitive fact of istream_iterator:
+  //    it doesn't read when you dereference it, but instead when you advance
+  //    (or construct) it.
+  auto it{istream_iterator<T>(cin)};
+  advance(it, n - 1, istream_iterator<T>{});
+}
+
+// Fold expression, parameter
+// packshttps://www.scs.stanford.edu/~dm/blog/param-pack.html
+// (See testGetInput for usage)
+template <semiregular ...T>
+auto getInput() {
+  tuple<T...> tpl;
+  apply([](auto &...args) { (cin >> ... >> args); }, tpl);
+  assert(cin.good() && "Input failure");
+  return tpl;
+}
+
+template <semiregular T>
+vector<T> getInput(signed_integral auto n) {
+  vector<T> vec(n);
+  copy_n(istream_iterator<T>(cin), n, begin(vec));
+  return vec;
+}
+
+vector<int> splitDigitsReversed(integral auto n) {
+  vector<int> v;
+  while (n) {
+    v.push_back(n % 10);
+    n /= 10;
+  }
+  return v;
+}
+
+vector<int> splitDigits(integral auto n) {
+  auto v{splitDigitsReversed(n)};
+  std::reverse(begin(v), end(v));
+  return v;
+}
+
+void solve();
+
+int main() {
+  try {
+    fast_io();
+    solve();
+    cout << endl;
+  } catch (...) {
+    cerr << "Catch-all handler" << endl;
+  }
+}
+
+bool isUnique(integral auto n) {
+  auto digits{splitDigitsReversed(n)};
+  set<int> uniqDigits{all(digits)};
+  return ssize(digits) == ssize(uniqDigits);
+}
+
+void solve() {
+  auto [y] = getInput<int>();
+  cout << *find_if(iota(y + 1), [](auto n) { return isUnique(n); });
+}
