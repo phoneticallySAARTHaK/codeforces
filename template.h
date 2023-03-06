1eabd12cd8fece173e93bfc6e6f5526dc70fc0df
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-06 13:28:54 +0530

    Add sections and input concepts, overload operator>> for pair

--- template.h
+++ template.h
@@ -1,5 +1,10 @@
 #pragma once
+
+// Headers
 #include <bits/stdc++.h>
+//
+
+// using declarations
 using std::accumulate;
 using std::apply;
 using std::cerr;
@@ -16,27 +21,62 @@ using std::numeric_limits;
 using std::pair;
 using std::ref;
 using std::semiregular;
+using std::set;
 using std::setw;
 using std::signed_integral;
 using std::streamsize;
 using std::string;
 using std::tuple;
-using std::vector;
-using std::set;
 using std::unreachable_sentinel;
+using std::vector;
+using std::operator>>;
+using std::operator<<;
+//
 
+// using directives
 namespace rs = std::ranges;
 namespace vs = std::views;
 using namespace rs;
 using namespace vs;
+//
 
+// #defines
 #define endl '\n'
 #define YES "YES"
 #define NO "NO"
 #define ll long long
 #define cauto const auto
 #define all(x) std::begin(x), std::end(x)
+//
 
+// concepts
+template <typename T>
+concept istreamable = requires(T x, std::istream &is) { is >> x; };
+
+template <typename T>
+concept ostreamable = requires(T x, std::ostream &os) { os << x; };
+
+template <typename T>
+concept streamable = (istreamable<T> && ostreamable<T>);
+//
+
+// std specializations
+namespace std {
+
+template <istreamable T>
+inline istream &operator>>(istream &is, pair<T, T> &p) {
+  return is >> p.first >> p.second;
+}
+
+template <ostreamable T>
+inline ostream &operator<<(ostream &os, pair<T, T> &p) {
+  return os << p.first << p.second;
+}
+
+} // namespace std
+//
+
+// utility functions
 inline void fast_io() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
@@ -47,41 +87,41 @@ inline void clear(int delim = '\n') {
   cin.clear();
 }
 
-template <typename T = int, typename N = int>
-  requires is_fundamental<T>::value && is_integral<N>::value
+template <istreamable T = int, signed_integral N = int>
 void discard(N n = 1) {
   // https://stackoverflow.com/questions/62940748/why-stdistream-iterator-with-multiple-copy-n-always-writes-firs-value
   // This comes down to a perhaps unintuitive fact of istream_iterator:
   //    it doesn't read when you dereference it, but instead when you advance
   //    (or construct) it.
-  auto it{istream_iterator<T>(cin)};
+  istream_iterator<T> it{cin};
   advance(it, n - 1, istream_iterator<T>{});
 }
 
 // Fold expression, parameter
 // packshttps://www.scs.stanford.edu/~dm/blog/param-pack.html
 // (See testGetInput for usage)
-template <semiregular ...T>
-auto getInput() {
+template <istreamable ...T>
+auto input() {
   tuple<T...> tpl;
   apply([](auto &...args) { (cin >> ... >> args); }, tpl);
   assert(cin.good() && "Input failure");
   return tpl;
 }
 
-template <semiregular T>
-vector<T> getInput(signed_integral auto n) {
+template <semiregular T> vector<T>
+input(signed_integral auto n) {
   vector<T> vec(n);
   copy_n(istream_iterator<T>(cin), n, begin(vec));
+  assert(cin.good() && "Input failure");
   return vec;
 }
 
 vector<int> splitDigitsReversed(integral auto n) {
   vector<int> v;
-  while (n) {
+  do {
     v.push_back(n % 10);
-    n /= 10;
   }
+  while (n /= 10);
   return v;
 }
 
@@ -90,3 +130,4 @@ vector<int> splitDigits(integral auto n) {
   std::reverse(begin(v), end(v));
   return v;
 }
+//
