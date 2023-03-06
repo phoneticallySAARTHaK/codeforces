a14394bc977e85dd801498634f36551b8829b22d
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-06 19:09:50 +0530

    Solve 344A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,28 @@
 #include "template.h"
 
+// Make a State Machine for storing state of the magnet string
+struct FSM {
+  char poles[2]{};
+  int parts{1};
+
+  void operator+=(const char ch) {
+    poles[0] = poles[1];
+    poles[1] = ch;
+    if (poles[0] == poles[1]) {
+      parts++;
+     }
+  }
+
+  operator int() {
+    return parts;
+  }
+};
+
 void solve() {
-  //
+  discard<int>();
+  FSM state{};
+  using ist = istream_iterator<char>;
+  for_each(ist{cin}, ist{},
+           [&state](cauto ch) { state += ch; });
+  cout << state;
 }
