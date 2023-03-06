76921304f8fb7291a35368d44322b48be9c9d584
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-06 21:02:24 +0530

    solve 61A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,12 @@
 #include "template.h"
 
 void solve() {
-  //
+  cauto [n1, n2] = input<string, string>();
+  auto size(ssize(n1));
+  string res;
+  res.reserve(size);
+  for (auto i{0}; i < size; i++) {
+    res += (n1[i] != n2[i]) + '0';
+  }
+  cout << res;
 }
