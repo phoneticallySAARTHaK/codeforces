7e70e13ed78ec1beaab7bbd6f5375106a237b3f0
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-06 20:50:02 +0530

    solve 200B

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,8 @@
 #include "template.h"
 
 void solve() {
-  //
+  cauto [n] = input<int>();
+  using ist = istream_iterator<int>;
+  cauto vol{accumulate(ist(cin), ist{}, 0.0)};
+  cout << vol / n;
 }
