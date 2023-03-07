bc3bc4a424ac4b7a5b1762564cb81e52993df82b
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-07 23:38:02 +0530

    solve 520A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,8 @@
 #include "template.h"
 
 void solve() {
-  //
+  discard<int>();
+  vector<bool> v(26);
+  for_each(isr<char>(), [&v](let ch) mut { v[tolower(ch) - 'a'] = true; });
+  cout << (any_of(v, falsy) ? NO : YES);
 }
