dd530b282a6f1c8bb4280f4375a42be67cdc0070
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-07 12:34:20 +0530

    solve 144A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,19 @@
 #include "template.h"
 
 void solve() {
-  //
+  auto [n] = input<int>();
+  auto v{input<int>(n)};
+  auto [min, max] = minmax_element(v);
+
+  auto max_pos {find(v, *max)};
+  auto rng = {*min};
+  auto min_pos_r {find_end(v, rng)};
+
+  auto minDistanceFromEnd {distance(end(min_pos_r), end(v))};
+  auto minDistanceFromBegin{distance(begin(v), begin(min_pos_r))};
+  auto maxDistanceFromBegin{distance(begin(v), find(v, *max))};
+
+  if (minDistanceFromBegin < maxDistanceFromBegin) minDistanceFromEnd--;
+
+  cout << maxDistanceFromBegin + minDistanceFromEnd;
 }
--- submission.cpp
+++ submission.cpp
@@ -185,3 +185,33 @@ vector<int> splitDigits(integral auto n) {
 //
 
 #endif // TEMPLATE_H
+
+void solve();
+
+int main() {
+  try {
+    fast_io();
+    solve();
+    cout << endl;
+  } catch (...) {
+    cerr << "Catch-all handler" << endl;
+  }
+}
+
+void solve() {
+  auto [n] = input<int>();
+  auto v{input<int>(n)};
+  auto [min, max] = minmax_element(v);
+
+  auto max_pos {find(v, *max)};
+  auto rng = {*min};
+  auto min_pos_r {find_end(v, rng)};
+
+  auto minDistanceFromEnd {distance(end(min_pos_r), end(v))};
+  auto minDistanceFromBegin{distance(begin(v), begin(min_pos_r))};
+  auto maxDistanceFromBegin{distance(begin(v), find(v, *max))};
+
+  if (minDistanceFromBegin < maxDistanceFromBegin) minDistanceFromEnd--;
+
+  cout << maxDistanceFromBegin + minDistanceFromEnd;
+}
