21547110ce721e9726d3b27b50e8ce54798e2414
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-07 10:35:06 +0530

    use lvalue lambdas, becuase rvalues don't work with ranges

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,9 @@
 #include "template.h"
 
 void solve() {
-  //
+  cauto [n] = input<int>();
+  vector<bool> levels(n, false);
+  using ist = istream_iterator<int>;
+  for_each(ist{cin}, ist{}, [&levels](cauto el) { levels[el - 1] = true; });
+  cout << (any_of(levels, falsy) ? "Oh, my keyboard!" : "I become the guy." ) ;
 }
