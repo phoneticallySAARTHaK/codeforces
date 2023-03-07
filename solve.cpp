2572b997e2dd91038b07038c1b9b352b27f658aa
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-07 12:50:57 +0530

    solve 1328A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,10 @@
 #include "template.h"
 
 void solve() {
-  //
+  discard<int>();
+  using ist = istream_iterator<pair<int, int>>;
+  for_each(ist{cin}, ist{}, [](cauto &p) {
+    cauto[a, b] = p;
+    cout << (a % b == 0 ? 0 : b - a % b) << endl;
+  });
 }
