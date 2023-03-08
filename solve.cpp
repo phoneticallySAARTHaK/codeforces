6c162b975f876950763e65cc4581b4852da528c5
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-08 22:47:50 +0530

    785A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,29 @@
 #include "template.h"
 
+fn getFaces(sv shape) {
+  if (shape == "Tetrahedron")
+    return 4;
+
+  if (shape == "Cube")
+    return 6;
+
+  if (shape == "Octahedron")
+    return 8;
+
+  if (shape == "Dodecahedron")
+    return 12;
+
+  if (shape == "Icosahedron")
+    return 20;
+
+  assert(false && "not a shape");
+  return 0;
+}
+
 fn solve() -> void {
-  //
+  discard<int>();
+  int faces{};
+  for_each(isr<string>(),
+           [&faces](auto &shape) mut { faces += getFaces(shape); });
+  cout << faces;
 }
--- submission.cpp
+++ submission.cpp
@@ -231,3 +231,43 @@ vector<int> splitDigits(integral auto n) {
 //
 
 #endif // TEMPLATE_H
+
+fn getFaces(sv shape) {
+  if (shape == "Tetrahedron")
+    return 4;
+
+  if (shape == "Cube")
+    return 6;
+
+  if (shape == "Octahedron")
+    return 8;
+
+  if (shape == "Dodecahedron")
+    return 12;
+
+  if (shape == "Icosahedron")
+    return 20;
+
+  assert(false && "not a shape");
+  return 0;
+}
+
+fn solve() -> void {
+  discard<int>();
+  int faces{};
+  for_each(isr<string>(),
+           [&faces](auto &shape) mut { faces += getFaces(shape); });
+  cout << faces;
+}
+
+void solve();
+
+fn main() -> int {
+  try {
+    fast_io();
+    solve();
+    cout << endl;
+  } catch (...) {
+    cerr << "Catch-all handler" << endl;
+  }
+}
