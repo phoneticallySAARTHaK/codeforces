f3c87774b8ebdeec80558c4dda685cf64f00389c
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-08 22:14:10 +0530

    solve 996A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,12 @@
 #include "template.h"
 
-fn solve() {
-  //
+fn solve() -> void {
+  int v[]{100, 20, 10, 5, 1};
+  auto [n] = input<int>();
+
+  cout << accumulate(ALL(v), 0, [money = n](let acc, let bill) mut {
+    auto t = acc + (money / bill);
+    money %= bill;
+    return t;
+  });
 }
