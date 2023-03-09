168b8cac7e6e3329828e6920d82fe6def24c70a6
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-09 20:48:23 +0530

    solve 141A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,22 @@
 #include "template.h"
 
+fn freqFill(auto& arr, let& str) {
+  for_each(str, [&arr](let ch) mut { arr[alphIndex(ch)]++; });
+}
+
 fn solve() -> void {
-  //
+  auto [s1, s2, s3] = input<string, string, string>();
+  array<int, 26> original{}, jumbled{};
+
+  for (let& str : {s1, s2}) freqFill(original, str);
+  freqFill(jumbled , s3);
+
+  for (int i{}; let f : original) {
+    if (jumbled[i] != f) {
+      cout << NO;
+      return;
+    }
+    i++;
+  }
+  cout << YES;
 }
