6d1bfd13aa9bed221241e069fc5954b2fa03ad68
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-09 22:25:24 +0530

    solve 427A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,13 @@
 #include "template.h"
 
 fn solve() -> void {
-  //
+  discard<int>();
+  cout << accumulate(CIN(int), 0, [manpw = 0](let acc, let ev) mut {
+    if (ev == -1 && manpw == 0)
+      return acc + 1;
+    else {
+      manpw += ev;
+      return acc;
+    }
+  });
 }
