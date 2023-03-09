a669940ce2bc3723d158365432f3cde22805aa70
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-09 21:37:41 +0530

    solve 155A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,26 @@
 #include "template.h"
 
 fn solve() -> void {
-  //
+  discard<int>();
+  int max{}, min{}, n{};
+  cin >> n;
+  max = min = n;
+
+  auto tracker = [] (int& max, int& min, let n) mut {
+    if (n > max) {
+      max = n;
+      return true;
+    } else if (n < min) {
+      min = n;
+      return true;
+    }
+    return false;
+  };
+
+  cout << accumulate(CIN(int), 0, [&](let acc, let n) mut {
+    if (tracker(max, min, n)) {
+      return acc + 1;
+    }
+      return acc;
+  });
 }
