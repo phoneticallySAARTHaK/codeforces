be12320991f4b51550ed5ad697b6eb92dce0589e
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-14 00:45:39 +0530

    solve 1409A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,11 @@
 #include "template.h"
 
 fn solve() -> void {
-  //
+  auto [t] = input<int>();
+  while (t--) {
+    auto [a, b] = input<int, int>();
+    auto [min, max] = minmax(a, b);
+    auto diff = max - min;
+    output(diff / 10 + static_cast<bool>(diff % 10), NL);
+  }
 }
