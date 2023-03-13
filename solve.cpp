2af7cf198355e501447971fe026907185a17bda1
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-14 01:42:43 +0530

    solve 472A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,10 @@
 #include "template.h"
 
 fn solve() -> void {
-  //
+  auto [n] = input<int>();
+  if (even(n)) {
+    cout << 4 << ' ' << n - 4;
+  } else {
+    cout << 9 << ' ' << n - 9;
+  }
 }
