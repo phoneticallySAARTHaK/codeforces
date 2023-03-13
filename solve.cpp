6ada5dd7e7e59a17b505433c57de50a723c46bf6
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-13 23:39:07 +0530

    solve 1154A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,11 @@
 #include "template.h"
 
 fn solve() -> void {
-  //
+  auto v{input<int>(4)};
+  sort(v, greater{});
+  auto numbers = v
+                   | drop(1)
+                   | vs::transform([abc = v.front()]
+                               (let n) mut { return abc - n; });
+  copy(numbers, ostream_iterator<int>(cout, " "));
 }
