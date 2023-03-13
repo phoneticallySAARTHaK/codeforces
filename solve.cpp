f6059e20c2f691bb8b1e6c298f8a24013f90e3bc
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-14 01:15:41 +0530

    solve 1399A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,19 @@
 #include "template.h"
 
 fn solve() -> void {
-  //
+  auto [t] = input<int>();
+  while (t--) {
+    auto [n] = input<int>();
+    auto v{input<int>(n)};
+    sort(v);
+    auto i{1};
+    for (; i < n; i++) {
+      if (v[i] - v[i - 1] > 1) {
+        cout << NO;
+        break;
+      }
+    }
+    if (i == n)
+     cout << YES;
+  }
 }
