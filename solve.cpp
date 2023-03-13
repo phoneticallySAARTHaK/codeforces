736303b6c84bf578fbb92faf119178fa2a3e2f2a
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-14 01:36:01 +0530

    solve 151A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,8 @@
 #include "template.h"
 
 fn solve() -> void {
-  //
+  auto [n, k, l, c, d, p, nl, np] =
+      input<int, int, int, int, int, int, int, int>();
+  vector v{(k * l) / nl, (c * d), p / np };
+  cout << *min_element(v) / n;
 }
