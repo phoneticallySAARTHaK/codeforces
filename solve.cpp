99305c3e13db459d5f3c97dd57d0cfcc15dd5338
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-13 22:19:25 +0530

    solve 1352A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,28 @@
 #include "template.h"
 
-fn solve() -> void {
-  //
+fn padZeroes(auto c) {
+  while (c--)
+    cout << '0';
+}
+
+fn solve()->void {
+  auto [t] = input<int>();
+  while (t--) {
+    auto [n] = input<string>();
+    let zeroCount{count(n, '0')};
+
+    cout << (ssize(n) - zeroCount) << '\n';
+    for_each(n, [power = ssize(n) - 1](let d) mut {
+      if (d == '0') {
+        power--;
+        return;
+      }
+
+      cout << d;
+      padZeroes(power);
+      power--;
+      cout << ' ';
+    });
+    cout << '\n';
+  }
 }
