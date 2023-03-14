bd761ebc6feef05857773c5cc7eb404dfd588e5a
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-14 21:01:58 +0530

    solve 432A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,11 @@
 #include "template.h"
 
 fn solve() -> void {
-  //
+  let max{5};
+  let [n, k] = input<int, int>();
+  let exp{input<int>(n)};
+  let eligible = accumulate(ALL(exp), 0, [maxVal = max - k](let acc, let p) {
+    return (acc + bool(p <= maxVal));
+  });
+  cout << (eligible / 3);
 }
