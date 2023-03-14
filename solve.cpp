987f81675366355a4834f50f3ea339329a94d940
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-14 22:11:52 +0530

    solve 1512A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,21 @@
 #include "template.h"
 
 fn solve() -> void {
-  //
+  auto [t] = input<int>();
+  while (t--) {
+    let [n] = input<int>();
+    auto arr{input<int>(n)};
+    auto dist{arr[0]}, i{1};
+
+    for (; i < n - 1; i++) {
+      if (dist != arr[i]) {
+        // dist is equal to last, but dist is not equal to ith element,
+        // means ith element is distinct
+        cout << ((dist == arr.back()) ? i + 1 : 1) << NL;
+        break;
+      }
+    }
+    // else, if no distinct element was found, it's the last
+    if (i == n - 1) cout << n << NL;
+  }
 }
