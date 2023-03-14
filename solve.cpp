0c98d42e70e0c9106ee7ac0f7ddeea6f99fe8a01
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-14 21:32:59 +0530

    solve 1367A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,14 @@
 #include "template.h"
 
 fn solve() -> void {
-  //
+  auto [t] = input<int>();
+  while(t--) {
+    auto [str] = input<string>();
+    cout << str[0];
+    auto last = ssize(str) - 1;
+    for (int i{1}; i < last; i += 2) {
+        cout << str[i];
+    }
+    cout << str.back() << NL;
+  }
 }
