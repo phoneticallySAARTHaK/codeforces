fcad790afbdf149a2a65b846188d325e65094798
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-14 23:02:50 +0530

    solve 490A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,25 @@
 #include "template.h"
 
 fn solve() -> void {
-  //
+
+  let [n] = input<int>();
+  array<vector<int>, 3> skillWiseCount{};
+
+  int i{1};
+  for_each(CIN(int), [&skillWiseCount, &i] (let el) mut {
+    skillWiseCount[el - 1].push_back(i++);
+  });
+
+  auto teamCount = ssize(min(skillWiseCount, {}, [](let& vec) { return ssize(vec);}));
+  output(teamCount);
+
+  while (teamCount--) {
+    for_each(skillWiseCount, [](auto& vec) {
+      cout << vec.back() << ' ';
+      vec.pop_back();
+    });
+    cout << NL;
+  }
+
 }
+
