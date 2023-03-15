a4b643dbad9f57d455ea6bad6463156815a1b995
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-15 19:55:10 +0530

    solve 381A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,29 @@
 #include "template.h"
 
 fn solve() -> void {
-  //
+  let [n] = input<int>();
+  auto cards{input<int>(n)};
+
+  auto left{begin(cards)};
+  auto right{--end(cards)};
+  pair<int, int> sum{};
+  int turn{}, current{};
+  while (distance(left, right) >= 0) {
+    if (*left > * right) {
+      current = *left;
+      left++;
+    }
+    else {
+      current = *right;
+      right--;
+    }
+
+    if (even(turn++)) {
+      sum.first += current;
+    }
+    else {
+      sum.second += current;
+    }
+  }
+  cout << sum;
 }
