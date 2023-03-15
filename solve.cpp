60615aa57c01c93d06b5123cedfce6269503e86f
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-15 21:22:42 +0530

    solve 1560A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,11 @@
 #include "template.h"
 
-fn solve() -> void {
-  //
+fn solve()->void {
+  auto [t] = input<int>();
+  let isLiked = [](let n) { return !(n % 3 == 0 || n % 10 == 3); };
+  while (t--) {
+    let[k] = input<int>();
+    auto seq = iota(1) | filter(isLiked) | take(k);
+    cout << max(seq) << NL;
+  }
 }
