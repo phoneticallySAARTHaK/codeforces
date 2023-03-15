dc11bfdcd2416628f6b4c34ad606d5e82753c0b6
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-15 21:50:39 +0530

    solve 703A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,25 @@
 #include "template.h"
 
-fn solve() -> void {
-  //
+fn solve()->void {
+  discard<int>();
+  using round_t = pair<int, int>;
+  let [mis, chris] = accumulate(CIN(round_t), make_pair(0, 0), [](let acc, let p) {
+    if (p.first > p.second) {
+      return make_pair(acc.first + 1, acc.second);
+    } else if (p.first < p.second) {
+      return make_pair(acc.first, acc.second + 1);
+    } else {
+      return acc;
+    }
+  });
+
+  if (mis > chris) {
+    cout << "Mishka";
+  }
+  else if (chris > mis) {
+    cout << "Chris";
+  }
+  else {
+    cout << "Friendship is magic!^^";
+  }
 }
