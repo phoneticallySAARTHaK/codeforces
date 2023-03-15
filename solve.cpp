d55687f977d55802817d661cd7e76518b77ff472
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-15 22:38:13 +0530

    solve 1692A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,11 @@
 #include "template.h"
 
 fn solve() -> void {
-  //
+  auto [t] = input<int>();
+  while (t--) {
+    auto dist{input<int>(4)};
+    let a = dist[0];
+    sort(dist, greater{});
+    output(distance(begin(dist), find(dist, a)));
+  }
 }
