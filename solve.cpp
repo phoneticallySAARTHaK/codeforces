ed2d58a6b1d419b1ced251311d88fca94e622930
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-16 02:06:37 +0530

    solve 1624A

--- solve.cpp
+++ solve.cpp
@@ -1,7 +1,10 @@
 #include "template.h"
 
-//#define TEST_CASES
+#define TEST_CASES
 
 inline fn solve()->void {
-  //
+  let [n] = input<int>();
+  let arr{input<int>(n)};
+  let [min, max] = minmax(arr);
+  output(max - min);
 }
