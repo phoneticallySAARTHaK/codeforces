ae4fe8ab384e45c766d70f9d855de57eded30805
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-15 22:28:20 +0530

    solve 1374A

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,11 @@
 #include "template.h"
 
 fn solve() -> void {
-  //
+  auto [t] = input<int>();
+  while (t--) {
+    let [x, y, n] = input<3, int>();
+    let rem {y % x};
+    let m{ (n - rem) / x};
+    output(m * x + rem);
+  }
 }
