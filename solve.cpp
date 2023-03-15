13393c9e9fae4ab8da1a61710a4660ccdeafdfb0
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-16 00:58:22 +0530

    solve 1367B

--- solve.cpp
+++ solve.cpp
@@ -1,7 +1,18 @@
 #include "template.h"
 
-//#define TEST_CASES
+#define TEST_CASES
 
-inline fn solve()->void {
-  //
+inline void solve() {
+  let[n] = input<int>();
+  auto arr{input<int>(n)};
+  auto mismatches{0}, balance{0};
+
+  for_each(arr, [&mismatches, &balance, i = 0](let a) mut {
+    if (!((even(i) && even(a)) || (!even(i) && !even(a)))) {
+      mismatches++;
+      balance += even(i) ? 1 : -1;
+    }
+    i++;
+  });
+  output(balance == 0 ? mismatches / 2 : -1);
 }
