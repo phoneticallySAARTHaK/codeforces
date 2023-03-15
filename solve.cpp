3f5be2d62e6c176acd5f07c24b7e1626fa923b43
Sarthak Batham <sarthakbatham26@gmail.com>
2023-03-16 02:14:57 +0530

    solve 431A

--- solve.cpp
+++ solve.cpp
@@ -1,7 +1,10 @@
 #include "template.h"
 
-//#define TEST_CASES
+// #define TEST_CASES
 
 inline fn solve()->void {
-  //
+  let[cals, strips] = input<array<int, 4>, string>();
+  output(accumulate(ALL(strips), 0, [&cals](let acc, let ch) {
+    return acc + cals[ch - '0' - 1];
+  }));
 }
